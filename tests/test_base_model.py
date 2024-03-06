1da8365b19bca12b3a221acda92c1a5e6f0b69e2
Pixel <piixel.1878@gmail.com>
2024-03-06 19:48:12 +0200

    Testing base_model_v0.1

---
+++ tests/test_base_model.py
@@ -0,0 +1,20 @@
+""" Testing BaseModel. """
+from models.base_model import BaseModel
+import unittest
+import uuid
+from models.base_model import BaseModel
+
+
+class testBaseModel(unittest.TestCase):
+    """ Tests the main model. """
+
+    def test_initilization(self):
+        my_model = BaseModel()
+        my_model.name = "My First Model"
+        my_model.number = 89
+        self.assertEqual([my_model.name, my_model.number],
+                         ["My First Model", 89])
+
+
+if __name__ == "__main__":
+    unittest.main()
