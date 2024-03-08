7c01442d004ca725eb56b786775cfca259b6ff92
Pixel <piixel.1878@gmail.com>
2024-03-08 21:48:10 +0200

    Console_0.0.1 (Create Method)

--- console.py
+++ console.py
@@ -1,31 +1,52 @@
 #!/usr/bin/env python3
 """ This module contains the HBNBCommand class """
 
-
 import cmd
 import sys
+from models.base_model import BaseModel
+from models.engine.file_storage import FileStorage  # Import FileStorage class
 
 
 class HBNBCommand(cmd.Cmd):
-    """ This class represents a command interpreter """
+    """ This class represents a command interpreter. """
+
+    def __init__(self, *args, **kwargs):
+        super().__init__(*args, **kwargs)
+        self.file_storage = FileStorage()  # Create an instance of FileStorage
 
     prompt = "(hbnb)"
 
     def do_quit(self, line):
-        """Quit command to exit the program\n"""
+        """Quit command to exit the program.\n"""
         return True
 
     def do_EOF(self, line):
-        """ Ctrl+d exits from the interpreter\n"""
+        """ Ctrl+d exits from the interpreter.\n"""
         return True
 
     def emptyline(self):
         pass
 
+    def do_create(self, line):
+        """ Creates a new instance of class name, saves it (to the JSON file),
+        and prints the ID\n"""
+        if not line:
+            print("** class name missing **")
+        else:
+            class_name = line.split()[0]
+            if class_name not in ["BaseModel"]:
+                print("** class doesn't exist **")
+            else:
+                new_instance = BaseModel()
+                # Use FileStorage instance to add new instance
+                self.file_storage.new(new_instance)
+                self.file_storage.save()  # Save the changes to file
+                print(new_instance.id)
+
 
 if __name__ == '__main__':
     if len(sys.argv) > 1:
-        # non interactive mode
+        # non-interactive mode
         HBNBCommand().onecmd(' '.join(sys.argv[1:]))
     else:
         HBNBCommand().cmdloop()
