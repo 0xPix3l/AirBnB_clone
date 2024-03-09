bc5879fc5a3259a787ce5b1b33dca94f695ec2c4
Pixel <piixel.1878@gmail.com>
2024-03-09 19:39:02 +0200

    adding user.py

---
+++ tests/test_user.py
@@ -0,0 +1,14 @@
+#!/usr/bin/python3
+
+
+"""Unittest for User."""
+
+
+import unittest
+from models.user import User
+from models.base_model import BaseModel
+
+
+class test_user(unittest.TestCase):
+    def test_data(self):
+        pass
