ed568ccbbafd0ddebd03df6feb4cb212f64a2fa7
Mohamed-Rabiaa <mohamedrabiaa1993@gmail.com>
2024-03-10 16:51:53 -0700

    updating test_user module

--- tests/test_models/test_user.py
+++ tests/test_models/test_user.py
@@ -9,6 +9,20 @@ from models.user import User
 from models.base_model import BaseModel
 
 
-class test_user(unittest.TestCase):
-    def test_data(self):
-        pass
+class TestUser(unittest.TestCase):
+    """ This class tests the User class """ 
+    def setUp(self):
+        """ Called before executing the test method """
+        self.my_user = User()
+        
+    def test_attributes(self):
+        """ Tests User attributes """
+        self.my_user.email = "au@gmail.com"
+        self.my_user.password = "12345678"
+        self.my_user.first_name = "John"
+        self.my_user.last_name = "Smith"
+
+        self.assertEqual(self.my_user.email, "au@gmail.com")
+        self.assertEqual(self.my_user.password, "12345678")
+        self.assertEqual(self.my_user.first_name, "John")
+        self.assertEqual(self.my_user.last_name, "Smith")
