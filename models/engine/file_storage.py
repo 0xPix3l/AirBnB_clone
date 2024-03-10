f8de95076d786003f6a3bbd5916243e53cb4ad20
Pixel <piixel.1878@gmail.com>
2024-03-10 18:21:23 +0200

    Updating to manage correctly serialization and deserialization

--- models/engine/file_storage.py
+++ models/engine/file_storage.py
@@ -1,9 +1,15 @@
 #!/usr/bin/env bash
 """ This module contains the FileStorage class """
 
-
 import json
 from os.path import exists
+from models.user import User
+from models.base_model import BaseModel
+from models.place import Place
+from models.state import State
+from models.city import City
+from models.amenity import Amenity
+from models.review import Review
 
 
 class FileStorage:
@@ -15,7 +21,7 @@ class FileStorage:
 
     def all(self):
         """
-         returns the dictionary __objects
+        returns the dictionary __objects
         """
         return type(self).__objects
 
@@ -24,7 +30,12 @@ class FileStorage:
         sets in __objects the obj with key <obj class name>.id
         """
         key = '{}.{}'.format(type(obj).__name__, obj.id)
-        type(self).__objects[key] = obj.to_dict()
+
+        # Check the class type and handle serialization
+        if type(obj).__name__ in ['User', 'Place', 'State', 'City', 'Amenity', 'Review']:
+            type(self).__objects[key] = obj.to_dict()
+        else:
+            type(self).__objects[key] = obj.to_dict()
 
     def save(self):
         """
@@ -47,4 +58,31 @@ class FileStorage:
         if exists(file):
             with open(file, encoding='utf-8') as f:
                 json_str = f.read()
-                type(self).__objects = json.loads(json_str)
+                # Load JSON data into objects
+                loaded_data = json.loads(json_str)
+
+                # Dictionary to map class names to corresponding class types
+                class_mapping = {
+                    'User': User,
+                    'Place': Place,
+                    'State': State,
+                    'City': City,
+                    'Amenity': Amenity,
+                    'Review': Review
+                }
+
+                # Iterate over the loaded data
+                for key, value in loaded_data.items():
+                    # Split the key to get class name and instance id
+                    class_name, obj_id = key.split()
+
+                    # Check if the class is in the class_mapping dictionary
+                    if class_name in class_mapping:
+                        # Create a new instance of the corresponding class
+                        new_instance = class_mapping[class_name](**value)
+                    else:
+                        # If class not found, default to creating a BaseModel instance
+                        new_instance = BaseModel(**value)
+
+                    # Update __objects dictionary
+                    type(self).__objects[key] = new_instance
